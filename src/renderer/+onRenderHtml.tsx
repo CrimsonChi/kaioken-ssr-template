d953b750360088d95e3c68f53f22c7c0993d643d
Robby6Strings <rausten93@gmail.com>
2024-03-24 06:52:34 +1300

    add public dir + favicon

--- src/renderer/+onRenderHtml.tsx
+++ src/renderer/+onRenderHtml.tsx
@@ -15,6 +15,7 @@ export const onRenderHtml: OnRenderHtmlAsync = async (
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <link rel="icon" href="/favicon.svg">
         <title>${getTitle(pageContext)}</title>
       </head>
       <body>
