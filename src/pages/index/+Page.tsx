ecc4f5ae92d698408647bc522aa348c996f8eb97
LankyMoose <rausten93@gmail.com>
2025-07-02 21:33:13 +1200

    chore: update dependencies to latest versions

--- src/pages/index/+Page.tsx
+++ src/pages/index/+Page.tsx
@@ -1,6 +1,21 @@
 import { Counter } from "$/components/Counter"
+import { useComputed, useSignal } from "kaioken"
+
+type RoomSocket = {
+  socket: WebSocket
+  send: (message: string) => void
+}
 
 export function Page() {
+  const roomId = useSignal(1)
+  const roomSocket = useComputed<RoomSocket>((prev) => {
+    prev?.socket.close()
+    const socket = new WebSocket(`ws://localhost:3000/ws/${roomId}`)
+    return {
+      socket,
+      send: (message: string) => socket.send(message),
+    }
+  })
   return (
     <>
       <h1 className="text-3xl md:text-4xl md:leading-normal font-bold text-center">
